464d904ac2dbd35f0632aa9eaa3954368b52ed90
Lorenzo Cingano <lorenzo@cingano.me>
2023-05-25 18:17:59 +0200

    Describing InitializationException

--- src/main/java/it/voxibyte/voxiapi/exception/InitializationException.java
+++ src/main/java/it/voxibyte/voxiapi/exception/InitializationException.java
@@ -1,5 +1,8 @@
 package it.voxibyte.voxiapi.exception;
 
+/**
+ * Exception thrown by VoxiPlugins when something failed to initialize
+ */
 public class InitializationException extends VoxiException {
     public InitializationException(String message) {
         super(message);
